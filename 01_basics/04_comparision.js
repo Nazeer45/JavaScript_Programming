6ac91e37797c44b1eb25b0cfe933d695d571b591
Nazeer <nazeerazam45@gmail.com>
2025-07-18 17:16:38 +0530

    comparision operators

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,21 @@
+// console.log(3 > 2) // true
+// console.log(3 >= 2) // true
+// console.log(3 < 2) // false
+// console.log(3 <= 2) // false
+// console.log(3 == 2) // false
+// console.log(3 != 2) // true
+
+// console.log("2" > 1) // true // string will be converted to number
+// console.log("02" > 1) // true // string will be converted to number
+// console.log("2" > "1") // true // string will be converted to number
+
+// console.log(null > 0) // false i.e null will be converted to 0
+// console.log(null == 0) // false i.e null will not be converted to 0
+// console.log(null >= 0) // true i.e null will be converted to 0
+
+// console.log(undefined == 0) // false
+// console.log(undefined > 0) // false
+// console.log(undefined < 0) // false
+
+// === strict equal to
+console.log(3 === "3") //false
