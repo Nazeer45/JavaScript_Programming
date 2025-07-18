6e91e6915c0e537888f325070e77341fb22223aa
Nazeer <nazeerazam45@gmail.com>
2025-07-18 16:25:00 +0530

    conversion operation

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -23,5 +23,35 @@ let booleanLoggedIn = Boolean(loggedIn)
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+// *********************** Operations ***********************
+
+let value = 3
+let negValue = -value
+// console.log(negValue)
+
+let str1 = "Hello"
+let str2 = " World"
+let str3 = str1 + str2
+// console.log(str3)
+
+// console.log("1"+ 2); // 12
+// console.log(1+ "2"); // 12
+// console.log("1"+ 2 + 2); // 122
+// console.log(1+ 2 + "2"); // 32
+// console.log(1+ "2" + 2); // 122
+
+
+// console.log((3 + 4) * 5 % 3);
+
+// console.log(+true); // 1
+// console.log(+""); // 0
+
+
+let gameScore = 100
+console.log(gameScore++); // 100
+console.log(++gameScore); // 102
+
+
