5b394c2acd5d611fb697d234e1e001e91fc9ae90
Nazeer <nazeerazam45@gmail.com>
2025-07-18 15:44:59 +0530

    conversion of datatypes

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,27 @@
+let score = "33ab"
+
+// console.log(typeof score) // string
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber) // number
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33ab" => NaN
+// true => 1; false => 0
+// "hello" => NaN
+// null => 0
+// undefined => NaN
+
+let loggedIn = 1
+let booleanLoggedIn = Boolean(loggedIn)
+// console.log(booleanLoggedIn)
+
+// 1 => true; 0 => false
+// "" => false
+// "hello" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
