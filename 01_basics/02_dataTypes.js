0342848c204cf58aa4b73c6ecd871acebdd3ddb8
Nazeer <nazeerazam45@gmail.com>
2025-07-18 10:51:38 +0530

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,20 @@
+"use strict"; //treat all JS code as newer version
+// alert(3+3) // we are using nodejs, not browser
+
+let name = "Nazeer"
+let age = 22
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint => intger without limit
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+console.log(typeof isLoggedIn) // boolean
+console.log(typeof name) // string 
+console.log(typeof undefined) // undefined
+console.log(typeof null) // object
